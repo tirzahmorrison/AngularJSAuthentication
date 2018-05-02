a29bfa44795a025352e83a4142180a7b9e667de8
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-05-01 22:07:05 -0400

    holy shit that was a lot of line ending conversions.

---
+++ AngularJSAuthentication.API/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace AngularJSAuthentication.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
